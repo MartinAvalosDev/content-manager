766af27cad0a5dc5315a5382bce50488e0fba1a5
Martin Avalos <MAAvalos2@teco.com.ar>
2023-09-24 01:08:13 -0300

    feat: improve request validations using ValidationPipe globaly

--- src/content/dtos/create-content.dto.ts
+++ src/content/dtos/create-content.dto.ts
@@ -1,17 +1,55 @@
+import { IsString, IsNumber, IsNotEmpty } from "class-validator";
+
 export class CreateContentDto{
+    @IsNotEmpty()
+    @IsString()
     readonly title: string;
+
+    @IsNumber()
     readonly episode_id: number;
+
+    @IsNotEmpty()
+    @IsString()
     readonly opening_crawl: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly director: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly producer: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly release_date: string;
+
+    @IsNotEmpty()
     readonly characters: [string];
-    readonly planets: [string];
-    readonly starships: [string];
-    readonly vehicles: [string];
-    readonly species: [string];
-    readonly created: string;
-    readonly edited: string;
-    readonly url: string;
+
     
+    @IsNotEmpty()
+    readonly planets: [string];
+
+    
+    @IsNotEmpty()
+    readonly starships: [string];
+    
+    @IsNotEmpty()
+    readonly vehicles: [string];
+    
+    @IsNotEmpty()
+    readonly species: [string];
+
+    @IsNotEmpty()
+    @IsString()
+    readonly created: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly edited: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly url: string;    
 }
--- src/content/dtos/update-content.dto.ts
+++ src/content/dtos/update-content.dto.ts
@@ -1,17 +1,38 @@
+import { IsString, IsNumber } from "class-validator";
+
 export class UpdateContentDto{
+    @IsString()
     readonly title: string;
+
+    @IsNumber()
     readonly episode_id: number;
+
+    @IsString()
     readonly opening_crawl: string;
+
+    @IsString()
     readonly director: string;
+
+    @IsString()
     readonly producer: string;
+
+    @IsString()
     readonly release_date: string;
+
+    //no aplica en arrays para updatear contents
     readonly characters: [string];
     readonly planets: [string];
     readonly starships: [string];
     readonly vehicles: [string];
     readonly species: [string];
+
+    @IsString()
     readonly created: string;
+
+    @IsString()
     readonly edited: string;
+
+    @IsString()
     readonly url: string;
     
 }
